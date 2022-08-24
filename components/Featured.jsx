59e1cd55b56fd6d52422dcd2784b0d1889dfca83
Murat Can ŞAHİN <muratcansahin10@gmail.com>
2022-08-24 10:12:11 +0300

    slider

---
+++ components/Featured.jsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import styles from "../styles/Featured.module.css";
+import { useState } from "react";
+const Featured = () => {
+  const [index, setIndex] = useState(0);
+  const images = [
+    "/img/featured.jpg",
+    "/img/featured2.jpg",
+    "/img/featured3.jpg",
+  ];
+  const handleArrow = (direction) => {
+    if (direction === "l") {
+      setIndex(index !== 0 ? index - 1 : 2);
+    }
+    if (direction === "r") {
+      setIndex(index !== 2 ? index + 1 : 0);
+    }
+  };
+  console.log(index);
+  return (
+    <div className={styles.container}>
+      <div
+        className={styles.arrowContainer}
+        style={{ left: 0 }}
+        onClick={() => handleArrow("l")}
+      >
+        <Image src="/img/arrowl.png" layout="fill" />
+      </div>
+      <div
+        className={styles.wrapper}
+        style={{ transform: `translateX(${-100 * index}vw) ` }}
+      >
+        {images.map((img, i) => (
+          <div className={styles.imgContainer}>
+            <Image src={img} key={i} layout="fill" />
+          </div>
+        ))}
+      </div>
+      <div
+        className={styles.arrowContainer}
+        style={{ right: 0 }}
+        onClick={() => handleArrow("r")}
+      >
+        <Image src="/img/arrowr.png" layout="fill" />
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
